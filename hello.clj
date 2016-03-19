3f5b8a7832400269635fccaac4d8c1cf12feff60
nakamuraPriv <t1014@xpost.plala.or.jp>
2016-03-19 19:31:26 +0900

    「ゲームを始めます！！」とコンソールに表示します

--- hello.clj
+++
@@ -1,4 +0,0 @@
-(.toUpperCase "hello")
-(str 1 2 nil 3)
-(Character/toUpperCase \s)
-(apply str (interleave "Attack at midnight" "The purple elephant chortled"))
---
+++ lifegame/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lifegame
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
